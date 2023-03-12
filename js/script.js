4b2722f8dc8767ba1079cf7804b115a4fd044251
kelsie_b <klsbrwn94@gmail.com>
2023-03-12 15:59:13 -0400

    Add button style click change with image removal

--- js/script.js
+++ js/script.js
@@ -3,5 +3,15 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 //console.log(cat);
 button.addEventListener("click", function () {
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        //console.log("Yes!");
+        cat.classList.remove("show");
+        button.innerText = "Wait, don't go!";
+        button.classList.add("disappear");
+    } else {
+        //console.log("No!"); 
+        cat.classList.add("show");
+        button.innerText = "Get out of here, cat!";
+        button.classList.remove("disappear");
+    }
 });
